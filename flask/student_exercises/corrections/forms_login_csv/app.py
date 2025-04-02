ff2dd204ce5389bfbd4cc5399279389ab1aff7f0
Antonio Pisanello <antonio@apisanello.dev>
2025-04-02 16:27:48 +0200

    Add correction for login_csv

--- flask/student_exercises/corrections/forms_login_csv/app.py
+++ flask/student_exercises/corrections/forms_login_csv/app.py
@@ -16,26 +16,49 @@ def login():
     pwd: str = request.form.get("tbx_pwd")
 
     # TODO: Read csv file to get the list of users
-    # TODO: for each user check if the login match the current user
-    # TODO: if login match return "Login Successful" otw "Logi Unsuccessful"
-
-    if email == "antonio@test.com" and pwd == "1234567890":
-      return "Login Successful"
-    else:
-      return "Login Unsuccessful"
-  else:
-    return render_template("login/login.html")
+    with open(CSV_FILE_PATH, "r") as csv_file:
+      reader = csv.DictReader(csv_file)
+      for row in reader:
+        # TODO: for each user check if the login match the current user
+        if email == row["email"] and pwd == row["password"]:
+          # TODO: if login match return "Login Successful" otw "Logi Unsuccessful"
+          return "Login Successful" 
+    return "Login Unsuccessful"
+  
+  return render_template("login/login.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
   # TODO: check if we are comming from GET or POST method
-  # IF method == "GET" return render template
-  # If method == "POST", we want to insert the user in the csv file
+  if request.method == "POST":
+    # If method == "POST", we want to insert the user in the csv file
+    email: str = request.form.get("tbx_email")
+    pwd: str = request.form.get("tbx_pwd")
+    pwd_repeat: str = request.form.get("tbx_pwd_repeat")
+
     # 1. Check if both password match if no -> return "Password doesn't match"
-    # 2. Insert the email and password in the csv file
-    # 3. return "Register successful"
+    if pwd != pwd_repeat:
+      return "Password mismatch"
+
     # BONUS: before inserting new user check if eamil already exists in csv
-      # if email already exists return "Email already in csv file"
+    with open(CSV_FILE_PATH, "r") as csv_file:
+      reader = csv.reader(csv_file)
+      next(reader)
+      for row in reader:
+        if row[0] == email:
+          # if email already exists return "Email already in csv file"
+          return "Email already in csv file"
+
+    # 2. Insert the email and password in the csv file
+    with open(CSV_FILE_PATH, mode="a") as csv_file:
+      csv_writer = csv.writer(csv_file)
+      csv_writer.writerow([email, pwd])
+
+    # 3. return "Register successful"
+    return "Register successful"
+  
+
+  # IF method == "GET" return render template
   return render_template("login/register.html")
 
 if __name__ == "__main__":
