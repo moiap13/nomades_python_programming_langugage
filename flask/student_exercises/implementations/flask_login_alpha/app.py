fd2d6ee326dfd3228a9cc05fd8ad9ed854d5ef96
Antonio Pisanello <antonio@apisanello.dev>
2025-08-12 12:45:15 +0200

    Add flask login alpha exercise

--- flask/student_exercises/implementations/flask_login_alpha/app.py
+++ flask/student_exercises/implementations/flask_login_alpha/app.py
@@ -1,54 +1,66 @@
 import os
+import csv
 
-import pandas as pd
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 CURR_DIR: str = os.path.dirname(__file__)
-CSV_FILE: str = os.path.join(CURR_DIR, 'users.csv')
+CSV_FILE: str = os.path.join(CURR_DIR, "users.csv")
+
 
 @app.route("/")
 def index():
-  return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-  """
-  Register function should add a new user to the users.csv file
-  """
-  if request.method == "POST":
-    # TODO: get the user uid from form
-    # TODO: get the user password from form
-    # TODO: get the user password 2 from form
+    if request.method == "POST":
+        firstname: str = request.form["tbx_firstname"]
+        lastname: str = request.form["tbx_lastname"]
+        email: str = request.form["tbx_email"]
+        password: str = request.form["tbx_password"]
+        password_confirmation: str = request.form["tbx_password_confirmation"]
 
-    # TODO: Validate that the two passwords are the same
+        if (
+            (
+                not (
+                    firstname
+                    and lastname
+                    and email
+                    and password
+                    and password_confirmation
+                )
+            )
+            or ("@" not in email)
+            or (password != password_confirmation)
+        ):
+            return "Please review the form"
 
-    # TODO: Open in "a" mode the users csv file using the constant CSV_FILE
-    # TODO: Add the new user to the CSV file
+        # TODO: insert data in csv database
+        # TODO: BONUS: check if email is already in csv file
+        print(firstname, lastname, email, password, password_confirmation)
+        return "Form sent successfully"
+    else:
+        return render_template("login/register.html")
 
-    # TODO(BONUS): Check if user already exists in csv file, if so -> return "User already exists"
-    return f""
-  else:
-    return render_template("login/register.html")
-  
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
-  """
-  The login functiojn should authenticate a user using his uid and pwd
-  """
-  if request.method == "POST":
-    uid: str = request.form["uid"]
-    pwd: str = request.form["pwd"]
+    if request.method == "POST":
+        email: str = ...
+        password: str = ...
 
-    # TODO: Open in "r" mode the users csv file using the constant CSV_FILE
-    # TODO: Loop throught the lines and check if the current line is the user's one
-    # TODO: Check if password match, between users.csv password and the user's form password
-    # TODO: if login successful -> return "Login successful"
-    # TODO: otw, return "Wrong credentials"
-    return f"{uid} - {pwd}"
-  else:
-    return render_template("login/login.html")
+        # TODO: Open in "r" mode the users csv file using the constant CSV_FILE
+        # TODO: Loop throught the lines and check if the current line is the user's one based on email
+        # TODO: Check if password match
+        # TODO: if login successful -> return "Login successful"
+        # TODO: otw, return "Wrong credentials"
+        return "Login successfully"
+    else:
+        return render_template("login/login.html")
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
