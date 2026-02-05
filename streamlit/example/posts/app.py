9b402b05d9d8f6d2aa86f6c73288095598e00571
Antonio Pisanello <antonio@apisanello.dev>
2026-02-05 13:00:23 +0100

    Add streamlit example

---
+++ streamlit/example/posts/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Home", layout="wide", initial_sidebar_state="expanded")
+
+st.title("Welcome to the Blog analysis tool")
+
+st.page_link("pages/authors.py", label="Authors analysis")
+st.page_link("pages/posts.py", label="Post analysis")
