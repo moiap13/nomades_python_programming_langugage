11e072fce2eaa502607296752bd05749e38e2208
Antonio Pisanello <antonio@apisanello.dev>
2026-02-20 12:19:13 +0100

    Add flask login csv with images correction

---
+++ flask/student_exercises/corrections/flask_login_csv/blueprints/user.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+ROOT_DIR: str = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(ROOT_DIR)
+
+from flask import Blueprint, render_template, session, url_for
+
+from helpers.decorators import authenticated
+from helpers.filesystem import verify_upload_exists
+from database.csv_database import get_user_by_username
+from models.user import User
+
+user_bp = Blueprint("user", __name__, url_prefix="/user")
+
+
+@user_bp.route("/info")
+@authenticated
+def userinfo():
+    user: User = get_user_by_username(session["uid"])
+    assert user is not None
+
+    pp_path: str = (
+        url_for("static", filename="uploads/" + user.avatar)
+        if verify_upload_exists(user.avatar)
+        else user.get_ui_avatar_url()
+    )
+    return render_template("user/userinfo.html", user=user.to_dict(), pp_path=pp_path)
+
+
+@user_bp.route("/api/info")
+def api_userinfo():
+    user: User = get_user_by_username("moiap13")
+    assert user is not None
+
+    return user.to_dict(), 200
