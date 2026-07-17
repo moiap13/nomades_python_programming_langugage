32311664938a44d1c3d83a36b62d0e1f83de5459
Antonio Pisanello <antonio@apisanello.dev>
2026-07-17 08:31:16 +0000

    Add falsy truthy example

---
+++ python/example/truthy_falsy.py
@@ -0,0 +1,28 @@
+# a: int = 10
+
+# if a == 10: # real boolean value
+#   print("if is true")
+# else:
+#   print("if is false")
+
+# Falsy / Truthy
+# a: int = 0
+
+# if a: 
+#   print("if is true")
+# else:
+#   print("if is false")
+
+# s: str = "Antonio"
+
+# if s: 
+#   print("if is true")
+# else:
+#   print("if is false")
+
+l: list[str] = ["Antonio"]
+
+if l: 
+  print("if is true")
+else:
+  print("if is false")
